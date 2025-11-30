3292c193dcf59d67e581076904a9bcb920db8caa
Arnav-Cuian <arnavsachan68@gmail.com>
2025-11-30 15:00:51 +0530

    arnav-update

---
+++ report.h
@@ -0,0 +1,11 @@
+#ifndef REPORT_H
+#define REPORT_H
+
+#include "order.h"
+
+// generate some basic reports
+double avgDeliveryTime();
+void showAllOrders();
+int totalRevenue(); // fake logic: assume each order = 100 bucks
+
+#endif
